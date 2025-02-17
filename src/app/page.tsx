04a4f283b80ba79a82a048e2dcfbe0485452f701
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2025-02-17 20:51:02 -0300

    big bang: initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main>
-      <div>Hello world!</div>
+    <main className="flex flex-col items-center justify-center w-screen h-screen bg-zinc-600">
+      <h1 className="text-4xl font-bold text-zinc-100">Hello world!</h1>
     </main>
   );
 }
