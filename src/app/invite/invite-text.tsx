6b5c9414b31e36dd141227b9ede057f2f9b1a7cb
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2025-02-18 17:13:17 -0300

    feat: invite text

---
+++ src/app/invite/invite-text.tsx
@@ -0,0 +1,23 @@
+export function InviteText() {
+  return (
+    <>
+      <div className="space-y-2">
+        <h1 className="text-4xl font-semibold font-heading text-gray-100 leading-none">
+          Your registration has been confirmed
+        </h1>
+        <p className="">To access the event, access the link in your email.</p>
+      </div>
+      <div className="space-y-6">
+        <div className="space-y-3">
+          <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
+            Share the event
+          </h2>
+          <p className="text-gray-300">
+            Invite your friends to participate in the event, share the
+            experience and compete for prizes. The more the merrier!
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -1,6 +1,7 @@
 import logo from "@/assets/logo.svg";
 import Image from "next/image";
 import { InviteLinkInput } from "./invite-link-input";
+import { InviteText } from "./invite-text";
 import { Ranking } from "./ranking";
 import { Stats } from "./stats";
 
@@ -11,26 +12,7 @@ export default function InvitePage() {
     <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
       <div className="flex flex-col gap-10 w-full max-w-[550px]">
         <Image src={logo} alt="devstage" width={108.5} height={30} />
-
-        <div className="space-y-2">
-          <h1 className="text-4xl font-semibold font-heading text-gray-100 leading-none">
-            Your registration has been confirmed
-          </h1>
-          <p className="">
-            To access the event, access the link in your email.
-          </p>
-        </div>
-        <div className="space-y-6">
-          <div className="space-y-3">
-            <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
-              Share the event
-            </h2>
-            <p className="text-gray-300">
-              Invite your friends to participate in the event, share the
-              experience and compete for prizes. The more the merrier!
-            </p>
-          </div>
-        </div>
+        <InviteText />
         <InviteLinkInput inviteLink={inviteLink} />
         <Stats />
       </div>
