c1224aed79371225c64a47430be0db18f868e3a0
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2025-02-18 17:06:15 -0300

    feat: invite page layout

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,9 +1,13 @@
 import { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
 
-export function IconButton({ ...rest }: ComponentProps<"button">) {
+export function IconButton({ className, ...rest }: ComponentProps<"button">) {
   return (
     <button
-      className="p-1.5 text-blue bg-gray-500 font-semibold rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900"
+      className={twMerge(
+        "p-1.5 text-blue bg-gray-500 font-semibold rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900",
+        className
+      )}
       {...rest}
     />
   );
