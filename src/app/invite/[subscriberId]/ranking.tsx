251fa4148235d8822dce69f8a0dc3a3bb5f7f3da
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2025-02-21 10:33:27 -0300

    feat: ranking integration

--- src/app/invite/[subscriberId]/ranking.tsx
+++ src/app/invite/[subscriberId]/ranking.tsx
@@ -1,42 +1,38 @@
 import medalCooper from "@/assets/medal-cooper.svg";
 import medalGold from "@/assets/medal-gold.svg";
 import medalSilver from "@/assets/medal-silver.svg";
+import { getRanking } from "@/http/api";
 import Image from "next/image";
 
-export function Ranking() {
+export async function Ranking() {
+  const { ranking } = await getRanking();
+
+  const medalIcons = [medalGold, medalSilver, medalCooper];
+
   return (
     <div className="w-full max-w-[440px] space-y-5">
       <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
         Ranking
       </h2>
       <div className="space-y-4">
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">1&ordm;</span> | Lauren Ferreira
-          </span>
-          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-            1030
-          </span>
-          <Image src={medalGold} alt="" className="absolute top-0 right-8" />
-        </div>
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">2&ordm;</span> | John Doe
-          </span>
-          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-            1030
-          </span>
-          <Image src={medalSilver} alt="" className="absolute top-0 right-8" />
-        </div>
-        <div className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">3&ordm;</span> | Joe Doe
-          </span>
-          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-            1030
-          </span>
-          <Image src={medalCooper} alt="" className="absolute top-0 right-8" />
-        </div>
+        {ranking.map((rank, i) => (
+          <div
+            className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3"
+            key={rank.id}
+          >
+            <span className="text-sm text-gray-300 leading-none">
+              <span className="font-semibold">{i + 1}&ordm;</span> | {rank.name}
+            </span>
+            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+              {rank.score} pts
+            </span>
+            <Image
+              src={medalIcons[i]}
+              alt=""
+              className="absolute top-0 right-8"
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/app/invite/[subscriberId]/stats.tsx
+++ src/app/invite/[subscriberId]/stats.tsx
@@ -42,7 +42,7 @@ export async function Stats({ subscriberId }: StatsProps) {
 
       <div className="relative rounded-xl bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1">
         <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-          {position}&ordm;
+          {position ? `${position}°` : "--"}
         </span>
         <span className="text-sm text-gray-300 leading-none text-center">
           Ranking position
